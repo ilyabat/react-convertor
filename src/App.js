1882618810e751c6183339e377d5a793e4b223da
ilyabanan <ibatuk53@gmail.com>
2022-10-03 12:12:31 +0300

    converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
+
+  const ratesRef = React.useRef({});
+  const [fromCurrency, setFormCurrency] = React.useState('UAH')
+  const [toCurrency, setToCurrency] = React.useState('USD')
+  const [fromPrice, setFromPrice] = React.useState(0)
+  const [toPrice, setToPrice] = React.useState(1)
+
+
+  React.useEffect(() => {
+    fetch('https://cdn.cur.su/api/latest.json')
+      .then(res => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates
+        onChangeToPrice(1)
+
+      })
+  }, [])
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency]
+    const result = price * ratesRef.current[toCurrency]
+    setToPrice(result)
+    setFromPrice(value)
+
+  }
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result)
+    setToPrice(value)
+  }
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency])
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFormCurrency} onChangeValue={onChangeFromPrice} />
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice} />
     </div>
   );
 }
